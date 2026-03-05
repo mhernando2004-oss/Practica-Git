b484716aca4332984abf1a32fb0073626bbe9af4
Mario Suárez Valverde <m.suarez@ceroca.info>
2026-03-05 10:02:18 +0100

    Ejercicio2

---
+++ Ejercicio2/Ex2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+Funció importAmbIVA
+Pre: importSenseIVA >= 0.0
+Post: retorna importSenseIVA * 1.16
+*/
+double importAmbIVA(double importSenseIVA);
+
+/* 
+Acció calculaRendiment
+Pre: capital > 0.0, interes > 0.0
+Post: imprime el rendiment anual = capital * (interes / 100.0)
+*/
+void calculaRendiment(double capital, double interes);
+
+
